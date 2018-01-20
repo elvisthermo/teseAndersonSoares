b1506b978e1c691cd8d8eb93afc1fb922e52d1a0
elvis <elvisthermo@hotmail.com>
2018-01-19 22:05:59 -0300

    Teste FX no javaSwing
    e o teste de do javafx  em java swing e mais uma ideia de glyph

---
+++ TreemapGlyph/src/doutorado/tese/gui/TestFxIntoSwing.java
@@ -0,0 +1,63 @@
+package doutorado.tese.gui;
+
+import javafx.application.Platform;
+import javafx.embed.swing.JFXPanel;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
+public class TestFxIntoSwing {
+
+    private static void initAndShowGUI() {
+        // This method is invoked on the EDT thread
+        JFrame frame = new JFrame("Swing and JavaFX");
+        final JFXPanel fxPanel = new JFXPanel();
+        frame.add(fxPanel);
+        frame.setSize(500, 500);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        Platform.runLater(new Runnable() {
+            @Override
+            public void run() {
+                initFX(fxPanel);
+            }
+       });
+    }
+
+    private static void initFX(JFXPanel fxPanel) {
+        // This method is invoked on the JavaFX thread
+        Scene scene = createScene();
+        fxPanel.setScene(scene);
+    }
+
+    private static Scene createScene() {
+        Group  root  =  new  Group();
+        Scene  scene  =  new  Scene(root, Color.ALICEBLUE);
+        Rectangle rectangle = new Rectangle();  
+      
+      //Setting the properties of the rectangle 
+        rectangle.setX(70); 
+        rectangle.setY(150); 
+        rectangle.setWidth(300.0f); 
+        rectangle.setHeight(150.0f); 
+
+        root.getChildren().add(rectangle);
+
+        return (scene);
+    }
+    
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                initAndShowGUI();
+            }
+        });
+    }
+}
